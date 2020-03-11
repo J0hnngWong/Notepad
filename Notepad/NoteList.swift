e60c146dd58e034a67e673f01285ea5676830de2
王嘉宁 <wangjianing@imdada.cn>
2020-03-11 11:02:38 +0800

    present a add new note view

--- Notepad/NoteList.swift
+++ Notepad/NoteList.swift
@@ -9,34 +9,35 @@
 import SwiftUI
 
 struct NoteList: View {
+    @State private var isPresented = false
     @EnvironmentObject private var userData: UserDataManager
     
     let addButtonHeight: CGFloat = 50
     var body: some View {
-        
-        VStack {
-            NavigationView {
-                VStack {
-                    List {
-                        ForEach(userData.userNoteData) { note in
-                            NavigationLink(destination: NoteEditPage(title: note.title, detail: note.detail)) {
-                                NoteListCell(note: note)
-                            }
-                        }
-                        .onDelete { (indexSet) in
-                            self.userData.userNoteData.remove(atOffsets: indexSet)
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(userData.userNoteData) { note in
+                        NavigationLink(destination: NoteEditPage(title: note.title, detail: note.detail)) {
+                            NoteListCell(note: note)
                         }
                     }
-                    .navigationBarTitle(Text("NoteList"), displayMode: .large)
-                    .padding(.bottom, -addButtonHeight)
-                    
-                    Button(action: {
-                        
-                    }) {
-                        Text("Add")
+                    .onDelete { (indexSet) in
+                        self.userData.userNoteData.remove(atOffsets: indexSet)
                     }
-                    .frame(width: addButtonHeight, height: addButtonHeight, alignment: .center)
                 }
+                .navigationBarTitle(Text("NoteList"), displayMode: .large)
+                .padding(.bottom, -addButtonHeight)
+                
+                Button("Add") {
+                    self.isPresented = true
+                }
+                .sheet(isPresented: $isPresented, onDismiss: {
+                    self.isPresented = false
+                }) {
+                    NoteEditPage(title: "", detail: "")
+                }
+                .frame(width: addButtonHeight, height: addButtonHeight, alignment: .center)
             }
         }
     }
