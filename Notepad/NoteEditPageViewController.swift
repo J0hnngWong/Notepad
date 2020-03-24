6efdf0b24522bb6cde4880f29c81d0b2820e27b1
王嘉宁 <wangjianing@imdada.cn>
2020-03-24 23:16:34 +0800

    add a dismiss action on edit note page

--- Notepad/NoteEditPageViewController.swift
+++ Notepad/NoteEditPageViewController.swift
@@ -10,8 +10,25 @@ import Foundation
 import UIKit
 
 class NoteEditPageViewController: UIViewController {
+    
+    @IBOutlet weak var commitButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        assignEvents()
+    }
+}
+
+extension NoteEditPageViewController {
+    func assignEvents() {
+        commitButton.addTarget(self, action: #selector(commitButtonClickAction), for: .touchUpInside)
+    }
+    
+    @objc
+    func commitButtonClickAction() {
+        dismiss(animated: true) {
+            
+        }
     }
 }
